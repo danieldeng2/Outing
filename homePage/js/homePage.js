817728d670ea372446d8a7e3db9ef47479f7b180
HoangVu_Ubuntu_Inspiron <hhv19@ic.ac.uk>
2020-02-08 18:02:51 +0000

    Done fetching messages

--- homePage/js/homePage.js
+++ homePage/js/homePage.js
@@ -1,3 +1,18 @@
+$(document).ready(function(){
+  $.post(
+    "admin/loadEvents.php",
+    function(data,status) {loadEvents(data);}
+    );
+
+  loadEvents();
+    
+
+});
+
+function loadEvents(data){
+  if(data != undefined){
+  $( "#rowId" ).append(data);}
+}
 
 $( "#signUpForm" ).submit(
 function( event ) {
@@ -6,7 +21,31 @@ function( event ) {
     $.post(
         "admin/signUp.php",
         $(this).serialize(),
-        function(data,status) {alert(status);}
+        function(data,status) {alert(data);}
         );
   }
 );
+
+$( "#signInForm" ).submit(
+  function( event ) {
+      event.preventDefault();
+      $.post(
+          "admin/signIn.php",
+          $(this).serialize(),
+          function(data,status) {alert(data);}
+          );
+    }
+  );
+
+$( "#newEventForm" ).submit(
+  function( event ) {
+      event.preventDefault();
+      $.post(
+          "admin/newEvent.php",
+          $(this).serialize(),
+          function(data,status) {alert(status);}
+          );
+    }
+  );
+  
+
