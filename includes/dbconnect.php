e41657134e0f0d3823662ea1c25d1e9bfdb51317
kf619 <kf619@ic.ac.uk>
2020-02-08 14:25:44 +0000

    update includes

---
+++ includes/dbconnect.php
@@ -0,0 +1,14 @@
+#!/usr/bin/php
+<?php
+function db_connect() {
+  static $connection;
+  if (!isset($connection)) {
+    $config = parse_ini_file(__DIR__ . '/../private/config.ini');
+    $connection = pg_connect("host=".$config['host']." dbname=".$config['dbname']." user=".$config['user']." password=".$config['password']);
+  }
+
+  return $connection;
+}
+
+$db = db_connect();
+?>
