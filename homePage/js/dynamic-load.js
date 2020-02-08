8495ca18e3aae55e3a5edafd5952cb99b7013d75
HoangVu_Ubuntu_Inspiron <hhv19@ic.ac.uk>
2020-02-08 14:02:27 +0000

    dynamic message skeleton done

---
+++ homePage/js/dynamic-load.js
@@ -0,0 +1,46 @@
+// GENERATE FIRST BATCH OF MESSAGES
+//This will be where you do your SQL and PHP first
+
+$outMessagePrepend='<div class="outgoing_msg"><div class="sent_msg"><p>';
+$inMessagePrepend='<div class="incoming_msg"><div class="incoming_msg_img"> <img src="icons/profile.png" alt="sunil"></div><div class="received_msg"><div class="received_withd_msg"><p>';
+$messageAppend='</p><span class="time_date"> 11:01 AM | June 9</span></div></div>';
+
+for(var i=0;i<20;i++){
+    $('.inner').prepend('<div class="messages">First Batch messages<br/><span class="date">'+Date()+'</span> </div>');}
+
+
+$("#msg_history").scrollTop($("#msg_history")[0].scrollHeight);
+
+// Assign scroll function to chatBox DIV
+$('#msg_history').scroll(function(){
+    if ($('#msg_history').scrollTop() == 0){
+        // Display AJAX loader animation
+         $('#loader').show();
+
+      // Youd do Something like this here
+      // Query the server and paginate results
+      // Then prepend
+      /*  $.ajax({
+            url:'getmessages.php',
+            dataType:'html',
+            success:function(data){
+                $('.inner').prepend(data);
+            };
+        });*/
+        //BUT FOR EXAMPLE PURPOSES......
+        // We'll just simulate generation on server
+
+
+        //Simulate server delay;
+        setTimeout(function(){
+        // Simulate retrieving 4 messages
+        for(var i=0;i<4;i++){
+        $('.inner').prepend('<div class="messages">Newly Loaded messages<br/><span class="date">'+Date()+'</span> </div>');
+            }
+            // Hide loader on success
+            $('#loader').hide();
+            // Reset scroll
+            $('#msg_history').scrollTop(30);
+        },780);
+    }
+});
