ae9fe22d08b6e2f1b5639bf32274e6b92a8a3daa
HoangVu_Ubuntu_Inspiron <hhv19@ic.ac.uk>
2020-02-09 01:07:31 +0000

    Hopefully got dynamicChat to work

--- homePage/js/dynamicChatLoad.js
+++ homePage/js/dynamicChatLoad.js
@@ -1,49 +1,34 @@
 $(document).ready(function(){
-  $.post(
-    "admin/loadEvents.php",
-    function(data,status) {loadEvents(data);}
-    );
-
-  loadEvents();
-
   alert("this works");
 });
 
-function loadEvents(data){
-  if(data != undefined){
-  $( "#rowId" ).append(data);}
+$(document).ready(function(){
+  $("#msg_history").load("chat_functions/getMessagse.php");
+});
+
+// GENERATE FIRST BATCH OF MESSAGES
+//This will be where you do your SQL and PHP first
+function addMessageToChat(data) {
+  if (data != undefined)
+    $('#msg_history').append(data);
+  }
+  alert("fdsasdf");
 }
 
-$( "#signUpForm" ).submit(
-function( event ) {
-    event.preventDefault();
+function test() {
+  alert("this works");
+}
 
-    $.post(
-        "admin/signUp.php",
-        $(this).serialize(),
-        function(data,status) {alert(data);}
-        );
-  }
-);
 
-$( "#signInForm" ).submit(
-  function( event ) {
-      event.preventDefault();
-      $.post(
-          "admin/signIn.php",
-          $(this).serialize(),
-          function(data,status) {alert(data);}
-          );
-    }
-  );
+$("#msg_history").scrollTop($("#msg_history")[0].scrollHeight);
 
-$( "#newEventForm" ).submit(
-  function( event ) {
-      event.preventDefault();
-      $.post(
-          "admin/newEvent.php",
-          $(this).serialize(),
-          function(data,status) {alert(status);}
-          );
-    }
-  );
+// Assign scroll function to chatBox DIV
+$('#msg_history').scroll(function(){
+    if ($('#msg_history').scrollTop() == 0){
+        // Display AJAX loader animation
+         $('#loader').show();
+
+
+function resetMessage() {
+  document.getElementById("messageBar").reset();
+}
