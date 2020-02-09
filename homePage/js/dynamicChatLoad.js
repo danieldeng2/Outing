a8601d4d069d50f1423e6ae1ed5c26131d201a05
HoangVu_Ubuntu_Inspiron <hhv19@ic.ac.uk>
2020-02-09 01:13:07 +0000

    Adding alert to dynamicchatload to test

--- homePage/js/dynamicChatLoad.js
+++ homePage/js/dynamicChatLoad.js
@@ -6,41 +6,8 @@ $(document).ready(function(){
 
   loadEvents();
 
-  alert("this works");
+  alert("fdsf");
 });
-
-$(document).ready(function(){
-
-  $("#msg_history").load("chat_functions/getMessagse.php");
-
-});
-
-// GENERATE FIRST BATCH OF MESSAGES
-//This will be where you do your SQL and PHP first
-function addMessageToChat(data) {
-  if (data != undefined)
-    $('#msg_history').append(data);
-  }
-  alert("fdsasdf");
-}
-
-function test() {
-  alert("this works");
-}
-
-
-$("#msg_history").scrollTop($("#msg_history")[0].scrollHeight);
-
-// Assign scroll function to chatBox DIV
-$('#msg_history').scroll(function(){
-    if ($('#msg_history').scrollTop() == 0){
-        // Display AJAX loader animation
-         $('#loader').show();
-
-
-function resetMessage() {
-  document.getElementById("messageBar").reset();
-}
 
 function loadEvents(data){
   if(data != undefined){
