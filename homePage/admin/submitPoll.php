f771f2aec0a0cd335dea5a695860dda79a108fc2
Daniel Deng <danieldeng223@gmail.com>
2020-02-09 10:30:30 +0000

    New polls added.

--- homePage/admin/submitPoll.php
+++ homePage/admin/submitPoll.php
@@ -11,7 +11,7 @@ pg_query($db, $part1 .$pid. $part2 .$pid. $part3);
 pg_query($db, "UPDATE polls SET voteduser = voteduser || ".$_COOKIE["userid"]." WHERE pollid=".$_POST["pollId"]);
 
 $polling = pg_query($db, "SELECT polltitle, array_to_json(optionstext),array_to_json(voteduser),array_to_json(optionsresult)  FROM polls WHERE pollid=".$_POST["pollId"]);
-$j=0;
+
 while (($row = pg_fetch_array($polling))) {
     echo '<div class="col-lg-12 mt-4" style="margin: auto;">
       <div class="card h-100 mb-4">
@@ -19,13 +19,13 @@ while (($row = pg_fetch_array($polling))) {
           <h4 class="card-title">'.$row["polltitle"].'</h4>';
 
           $totalvote = array_sum(json_decode($row[3]));
-          
+          $j=0;
           foreach (json_decode($row[1]) as &$optionstext) {       
               $voteNum = (int) json_decode($row[3])[$j];     
-              if($j = $_POST["pollRadios"]) {$voteNum++;};
+              // if($j = $_POST["pollRadios"]) {$voteNum++;};
               echo '<h6 class="card-subtitle text-muted">'.$optionstext.'</h6>
               <div class="progress mb-3 mt-1" >
-                <div class="progress-bar" role="progressbar" style="width:'.((int) $voteNum/$totalvote*100).'%" aria-valuenow="'.$voteNum.'" aria-valuemin="0" aria-valuemax="'.$totalvote.'"></div>
+                <div class="progress-bar" role="progressbar" style="width:'.((int) ($voteNum==0?0:$voteNum/($totalvote)*100)).'%" aria-valuenow="'.$voteNum.'" aria-valuemin="0" aria-valuemax="'.$totalvote.'"></div>
               </div> ';
               $j++;
           };
