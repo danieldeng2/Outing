2be4bdd087fef0273ccedc2e4a95cc1f11924713
HoangVu_Ubuntu_Inspiron <hhv19@ic.ac.uk>
2020-02-09 02:54:48 +0000

    undefined test

--- homePage/js/dynamicChatLoad.js
+++ homePage/js/dynamicChatLoad.js
@@ -1,3 +1,8 @@
+  var incomingMessagePrepend='<div class="outgoing_msg"><div class="sent_msg"><p>';
+  var outgoingMessagePrepend='<div class="incoming_msg"><div class="incoming_msg_img"> <img src="icons/profile.png" alt="sunil"></div><div class="received_msg"><div class="received_withd_msg"><p>';
+  var messageTimePrepend='</p>';
+  var messageTimeAppend='</span></div></div>';
+
 $(document).ready(function(){
   $.post(
     "chat_functions/getMessages.php",
@@ -11,9 +16,8 @@ $(document).ready(function(){
 
 alert("dfsd");
 
-$("#msg_history").append("<span> got added </span>");
 
 function loadEvents(data){
   if(data != undefined){
-  $( "#msg_history" ).append(data);}
+  $( "#msg_history" ).append(incomingMessagePrepend + data + messageTimePrepend);}
 }
