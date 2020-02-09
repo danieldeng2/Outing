b2845ff1c1d79a30d304b8ae3465c7ca0cb0fc45
HoangVu_Ubuntu_Inspiron <hhv19@ic.ac.uk>
2020-02-09 02:51:21 +0000

    undefined test

--- homePage/js/dynamicChatLoad.js
+++ homePage/js/dynamicChatLoad.js
@@ -12,41 +12,8 @@ $(document).ready(function(){
 alert("dfsd");
 
 $("#msg_history").append("<span> got added </span>");
+
 function loadEvents(data){
   if(data != undefined){
-  $( "#rowId" ).append(data);}
+  $( "#msg_history" ).append(data);}
 }
-
-$( "#signUpForm" ).submit(
-function( event ) {
-    event.preventDefault();
-
-    $.post(
-        "admin/signUp.php",
-        $(this).serialize(),
-        function(data,status) {alert(data);}
-        );
-  }
-);
-
-$( "#signInForm" ).submit(
-  function( event ) {
-      event.preventDefault();
-      $.post(
-          "admin/signIn.php",
-          $(this).serialize(),
-          function(data,status) {alert(data);}
-          );
-    }
-  );
-
-$( "#newEventForm" ).submit(
-  function( event ) {
-      event.preventDefault();
-      $.post(
-          "admin/newEvent.php",
-          $(this).serialize(),
-          function(data,status) {alert(status);}
-          );
-    }
-  );
