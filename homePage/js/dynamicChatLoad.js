90e203424934c31b3059ebc4bbfd341816b6b476
HoangVu_Ubuntu_Inspiron <hhv19@ic.ac.uk>
2020-02-09 01:17:46 +0000

    testing addtoMessageChat function

--- homePage/js/dynamicChatLoad.js
+++ homePage/js/dynamicChatLoad.js
@@ -6,8 +6,16 @@ $(document).ready(function(){
 
   loadEvents();
 
+  $.post(
+    "chat_functions/getMessages.php",
+    function(data,status) {addMessageToChat(data);}
+  );
   alert("fdsf");
 });
+
+function addMessageToChat(data) {
+  $("msg_history").load(data);
+}
 
 function loadEvents(data){
   if(data != undefined){
