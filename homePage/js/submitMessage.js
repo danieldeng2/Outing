7de80649af646e9421c63c8962d2a8bed6bf81f9
HoangVu_Ubuntu_Inspiron <hhv19@ic.ac.uk>
2020-02-09 06:02:20 +0000

    put message done

---
+++ homePage/js/submitMessage.js
@@ -0,0 +1,18 @@
+var url = window.location.href;
+var idStart = url.indexOf("id=");
+var groupId = url.substring(idStart + 3, idStart + 4);
+
+$(document).ready(function(){
+
+});
+
+$("messageBar").submit(
+  function (event) {
+    event.preventDefault();
+    $.post(
+      "chat_functions/putMessages.php",
+      { message : $(this).serialize(), groupid : groupId },
+      function(data, status) {alert("message submitted");}
+    );
+  }
+);
